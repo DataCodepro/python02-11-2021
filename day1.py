17e2b0d1839545b8135d8a449120ffbf098879fc
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2021-12-07 17:29:07 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,26 @@
+'''Writing a pyton code'''
+print('hello world')
+NUMBER1 = 8
+NUMBER2 = 12.5
+WORD = 'Simeon'
+X = True
+
+WHOLENUMBER = 50 #uppercase naming stlye
+DECIMAL_NUMBER = 15.5 # snake naming stlye
+FIRSTNAME = 'Simeon' # camel naming style
+SECONDNAME = 'Taiwo' # Pascal naming style
+print(WHOLENUMBER)
+WORD  = 'we\'re brothers from the other side of the town'
+print(WORD)
+POEM ='oh what a great country,\na country filled with honey and milk,\nHope to always eat from the good of the land'
+print(POEM)
+print('\n')
+POEM2 = '''oh what a great country,
+a country filled with honey and milk,
+Hope to always eat from the good of the land
+'''
+print(POEM2)
+print('\n')
+print('python' + ' '+'course') #joining of string and string
+COURSE = 'Python'
+print('Hey', COURSE, 'is fun') # joining of variable and string
---
+++ day2.py
@@ -0,0 +1,20 @@
+price1= 25000
+price2 = 5000
+price3 = 85000
+report = 'based on the day activity i sold 5 pairs of shirt for {2}, also sold a shoe  for {0} and a suit for {1}'
+print(report.format(price1,price2,price3))
+
+print('\n')
+print(f'based on the day activity i sold 5 pairs of shirt for {price1}, also sold a shoe  for {price2} and a suit for {price3}')
+#You can convert an interger value and a floating value to string
+#You can convert an interger value to float
+# you can convert float to interger
+# you cannot convert a string to a float and interger except it is a number that is passed in a string
+num1 = float(15)
+print(num1)
+num2 = 15.5
+print(int(num2))
+word = 'p'
+#print(float(word))
+num3 = '10'
+print(float(num3))
